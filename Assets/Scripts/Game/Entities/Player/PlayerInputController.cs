0a4c3695991992a2ec819ba88903bc9f4988cfc3
Franzyd <firekfrank@gmail.com>
2021-11-26 12:56:35 +0100

    Remove Input Manager code

--- Assets/Scripts/Game/Entities/Player/PlayerInputController.cs
+++ Assets/Scripts/Game/Entities/Player/PlayerInputController.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(IMoveable))]
 public class PlayerInputController : MonoBehaviour
 {
+    [HideInInspector]
+    public Vector2 moveVal;
+
     private IMoveable _player;
     // Start is called before the first frame update
     void Start()
@@ -12,9 +16,17 @@ public class PlayerInputController : MonoBehaviour
         _player = GetComponent<IMoveable>();
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    //// Update is called once per frame
+    //void FixedUpdate()
+    //{
+    //    _player.Move(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
+    //}
+
+    void onMove(InputValue value)
     {
-        _player.Move(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
+        moveVal = value.Get<Vector2>();
+        Debug.Log("X: " + moveVal.x + ", Y: " + moveVal.y);
+        _player.Move(new Vector2(moveVal.x, moveVal.y));
     }
+
 }
