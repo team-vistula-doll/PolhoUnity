3602cc923c3aa97430cc4b50199dda7fa3d35546
Franzyd <firekfrank@gmail.com>
2022-06-02 22:37:52 +0200

    Create ParseStageEnemies method

---
+++ Assets/Scripts/Game/Entities/Enemies/EnemyStruct.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace EnemyStruct
+{
+    public struct Enemy
+    {
+        public int ID;
+        public string Name;
+        public float SpawnTime;
+        public Vector2 SpawnPosition;
+        public List<List<Vector2>> Waypoints;
+        public List<(float delay, int amount)> SpawnRepeats; //delay between spawns, amount of spawns; Optional
+    }
+}
